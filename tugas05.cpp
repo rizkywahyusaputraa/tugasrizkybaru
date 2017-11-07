ee69b6b5047700df1dc74804f324a760869e5fbb
rizkywahyusaputraa <32368070+rizkywahyusaputraa@users.noreply.github.com>
2017-11-08 00:58:18 +0800

    menambahkan tinggi dan berat

--- tugas05.cpp
+++ tugas05.cpp
@@ -7,4 +7,7 @@ int main()
 	cout<<"||----PROGRAM MENENTUKAN underweight, normal, atau overweight----||"<<endl;
     float bb;
     int tb;
+     cout << "Silahkan masukkan tinggi badan(cm) : "; cin >>tb;
+    cout << "Silahkan masukkan berat badan(kg) : " ;
+    cin >> bb;
 }
