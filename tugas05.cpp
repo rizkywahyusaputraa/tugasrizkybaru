e9225ce647daba3589823a332e58fb691fb027d6
rizkywahyusaputraa <32368070+rizkywahyusaputraa@users.noreply.github.com>
2017-11-08 00:59:21 +0800

    membuat if dan menampilkan underweight

--- tugas05.cpp
+++ tugas05.cpp
@@ -10,4 +10,6 @@ int main()
      cout << "Silahkan masukkan tinggi badan(cm) : "; cin >>tb;
     cout << "Silahkan masukkan berat badan(kg) : " ;
     cin >> bb;
+    if (bb<tb/2.5)
+       cout << "  Anda Mengalami : Underweight(kekurangan berat badan)" << endl;
 }
