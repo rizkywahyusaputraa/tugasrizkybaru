4bf6e044ce63437dbb2a5d99052a650a4ab1d8b3
rizkywahyusaputraa <32368070+rizkywahyusaputraa@users.noreply.github.com>
2017-11-08 00:33:09 +0800

    membuat variabel dan menampilkan inisialisasi lama kerja dan gaji

--- tugas03.cpp
+++ tugas03.cpp
@@ -2,5 +2,9 @@
 using namespace std;
 
 int main(){
-	
+float jk, le, gtot, gp, gb,ul, jam,pa;
+	cout<<"   PROGRAM GAJI BBERSIH KARYAWAN"<<endl;
+	cout<<""<<endl;
+	cout<<"Lama Kerja Karyawan    : "; cin>> jk;
+	cout<<"Gaji Perjam Karyawan   : "; cin>>gp;	
 }
